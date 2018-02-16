1fe367402cc34feaebeccbff03e1189ab9fa7d70
raymond <ventoso.ramon@gmail.com>
2018-02-16 10:14:13 -0500

    first

---
+++ UnitTestingWebAPI.Service/ArticleService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestingWebAPI.Data.Infrastructure;
+using UnitTestingWebAPI.Data.Repositories;
+using UnitTestingWebAPI.Domain;
+
+namespace UnitTestingWebAPI.Service
+{
+    // operations you want to expose
+    public interface IArticleService
+    {
+        IEnumerable<Article> GetArticles(string name = null);
+        Article GetArticle(int id);
+        Article GetArticle(string name);
+        void CreateArticle(Article article);
+        void UpdateArticle(Article article);
+        void DeleteArticle(Article article);
+        void SaveArticle();
+    }
+
+    public class ArticleService : IArticleService
+    {
+        private readonly IArticleRepository articlesRepository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public ArticleService(IArticleRepository articlesRepository, IUnitOfWork unitOfWork)
+        {
+            this.articlesRepository = articlesRepository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        #region IArticleService Members
+
+        public IEnumerable<Article> GetArticles(string title = null)
+        {
+            IEnumerable<Article> results;
+            if (string.IsNullOrEmpty(title))
+                results =  articlesRepository.GetAll();
+            else
+                results = articlesRepository.GetAll().Where(c => c.Title.ToLower().Contains(title.ToLower()));
+            return results;
+        }
+
+        public Article GetArticle(int id)
+        {
+            var article = articlesRepository.GetById(id);
+            return article;
+        }
+
+        public Article GetArticle(string title)
+        {
+            var article = articlesRepository.GetArticleByTitle(title);
+            return article;
+        }
+
+        public void CreateArticle(Article article)
+        {
+            articlesRepository.Add(article);
+        }
+
+        public void UpdateArticle(Article article)
+        {
+            articlesRepository.Update(article);
+        }
+
+        public void DeleteArticle(Article article)
+        {
+            articlesRepository.Delete(article);
+        }
+
+        public void SaveArticle()
+        {
+            unitOfWork.Commit();
+        }
+
+        #endregion
+    }
+}
